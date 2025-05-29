4e6918c4dcb0196de09d9a6e1cae2a1cb2398393
jji1016 <joeyji1016@gmail.com>
2025-05-29 11:45:31 +0900

    번역 동기 비동기

--- day04/trans02.py
+++ day04/trans02.py
@@ -1,6 +1,9 @@
 #구글에서 만든 라이브러리 있음  비동기 동작
 import googletrans
+import inspect
 import asyncio
+print(inspect.iscoroutinefunction(googletrans.Translator().translate))  
+
 async def main() :
     translator = googletrans.Translator()
 
@@ -8,10 +11,33 @@ async def main() :
     with open("day04/sing.txt","r",encoding="utf-8") as f :
        lines = [line.strip() for line in f if line.strip()] #리스트로 만들고
     
-    task = [translator.translate(line,dest="en") for line in lines]
+    task = [translator.translate(line,dest="zh-cn") for line in lines]
     result = await asyncio.gather(*task)
 
-    with open("day04/translate_en.txt","w",encoding="utf-8") as f :
+    with open("day04/translate_zh.txt","w",encoding="utf-8") as f :
         for original, translated in zip(lines,result) :
             f.write(f"{translated.text}\n")
 asyncio.run(main())
+
+#만약 print(inspect.iscoroutinefunction(googletrans.Translator().translate))   false라면 비동기 지원 안함...
+#아래쪽 코드 실행
+# import googletrans
+# import inspect
+# import asyncio
+# print(inspect.iscoroutinefunction(googletrans.Translator().translate))  
+# #만약 false이면
+
+# def main() :
+#     translator = googletrans.Translator()
+
+#     #파일 같은거 열때 작업이 끝나면 지가 알아서 닫아준다.
+#     with open("day04/sing.txt","r",encoding="utf-8") as f :
+#        lines = [line.strip() for line in f if line.strip()] #리스트로 만들고
+    
+#     #task = [translator.translate(line,dest="zh-cn") for line in lines]
+#     result = [translator.translate(line,dest="zh-cn") for line in lines]
+
+#     with open("day04/translate_zh.txt","w",encoding="utf-8") as f :
+#         for original, translated in zip(lines,result) :
+#             f.write(f"{translated.text}\n")
+# main()
